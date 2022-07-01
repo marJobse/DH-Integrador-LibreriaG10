631d7ccd10dff72aae212d72a7c315d6743cd3d4
mariana.jobse <jobsemarr@yahoo.com.ar>
2022-06-30 21:25:15 -0300

    ruta index y productDetail

--- libreria_G10/app.js
+++ libreria_G10/app.js
@@ -8,24 +8,11 @@ const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath));
 
 
-app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));
-
+app.listen(3030, () => console.log('Servidor corriendo en el puerto 3030'));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './views/index.html'));
 });
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/register.html'));
-
-});
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/login.html'));
-});
 app.get('/productDetail', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/productDetail.html'));
 });
-app.get('/productCart', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/productCart.html'));
-});
-
