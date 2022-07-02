bcd1a83ed55cc9fb1a7f7afd6e7875907a9a5edd
nachomat85 <ignaciomateu@gmail.com>
2022-07-02 18:00:59 -0300

    actualizaciones del header, footer (en progreso) y login

--- libreria_G10/app.js
+++ libreria_G10/app.js
@@ -17,6 +17,10 @@ app.get('/productDetail', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/productDetail.html'));
 });
 
-app.get('/registro', (req, res) => {
+app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
 });
