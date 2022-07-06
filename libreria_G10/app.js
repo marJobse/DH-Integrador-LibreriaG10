2cdb2b709b2910fbb71bcb54aba6d32f4073bc9c
nachomat85 <ignaciomateu@gmail.com>
2022-07-05 22:14:47 -0300

    subiendo header y footer responsivos, y pagina de product cart en progreso

--- libreria_G10/app.js
+++ libreria_G10/app.js
@@ -24,3 +24,7 @@ app.get('/register', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/login.html'));
 });
+
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/productCart.html'));
+});
