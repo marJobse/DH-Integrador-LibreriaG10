e29d1e35777f23a8de378226fe778bbf6794bcd1
nachomat85 <ignaciomateu@gmail.com>
2022-07-16 15:03:23 -0300

    actualizando rutas

--- libreria_G10/src/app.js
+++ libreria_G10/src/app.js
@@ -9,32 +9,29 @@ const cartRouter = require('./routes/cartRouter');
 
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT || 3030, () => console.log('Servidor corriendo en el puerto 3030'));
-
-
 app.use("/",mainRouter)
 app.use("/productDetail",productsRouter)
 app.use("/users",usersRouter)
-app.use("/productCart",cartRouter)
+app.use("/carrito",cartRouter)
+
+app.listen(process.env.PORT || 3030, () => console.log('Servidor corriendo en el puerto 3030'));
 
 
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, './views/index.html'));
+// });
+// app.get('/productDetail', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/views/productDetail.html'));
+// });
 
+// app.get('/register', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/views/register.html'));
+// });
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, './views/index.html'));
-});
-app.get('/productDetail', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/productDetail.html'));
-});
+// app.get('/login', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/views/login.html'));
+// });
 
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/register.html'));
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/login.html'));
-});
-
-app.get('/carrito', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/productCart.html'));
-});
+// app.get('/carrito', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/views/productCart.html'));
+// });
--- libreria_G10/src/routes/productRouter.js
+++ libreria_G10/src/routes/productRouter.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router()
 const productsController = require("../controllers/productsController")
 
-router.get("/productDetail", productsController.detail);
+router.get("/", productsController.detail);
 
 module.exports = router;
