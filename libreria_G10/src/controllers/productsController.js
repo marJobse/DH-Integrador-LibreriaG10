a1ad9ce4fa52e914e1f64afe14c22c709a025c3d
Maximo Segundo Fonrouge <msfonrouge@gmail.com>
2022-07-20 21:02:45 -0300

    cambios home y product edit

--- libreria_G10/src/controllers/productsController.js
+++ libreria_G10/src/controllers/productsController.js
@@ -4,6 +4,9 @@ const productsController = {
     detail: (req, res) => {
         res.render('productDetail')
     },
+    edit: (req, res) => {
+        res.render('productEdit')
+    },
 }
 
 module.exports = productsController;
