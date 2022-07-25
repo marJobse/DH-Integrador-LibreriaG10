eb44560c3455192858dd31719aec0d6fe6d84dd3
nachomat85 <ignaciomateu@gmail.com>
2022-07-25 18:32:04 -0300

    sincronizando products Controller

--- libreria_G10/src/controllers/productsController.js
+++ libreria_G10/src/controllers/productsController.js
@@ -1,14 +1,26 @@
+
+const express = require("express");
 const path = require('path');
+const fs = require('fs');
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const productsController = {
     detail: (req, res) => {
-        res.render('productDetail')
+        res.render('../views/products/productDetail.ejs')
     },
     list: (req, res) => {
-        res.render('productList')
-    edit: (req, res) => {
-        res.render('productEdit')
+        res.render('../views/products/productList.ejs')
     },
+    edit: (req, res) => {
+        res.render('../views/products/productEdit.ejs')
+    },
+    delete: (req, res) => {
+        res.render('../views/products/productConfirmacionEliminar.ejs')
+    },
+    verBase: (req, res) => {
+        res.render('../views/products/all.ejs', { products })
+    }
 }
 
 module.exports = productsController;
