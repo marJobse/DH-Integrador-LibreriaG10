5b8142120725958d16bc88bd62e32770e2a7865b
mariana.jobse <jobsemarr@yahoo.com.ar>
2022-07-27 21:45:01 -0300

    /create get y post

--- libreria_G10/src/controllers/productsController.js
+++ libreria_G10/src/controllers/productsController.js
@@ -19,6 +19,30 @@ const productsController = {
     },
     verBase: (req, res) => {
         res.render('../views/products/all.ejs', { products })
+    },
+    create: (req, res) => {
+        res.render('../views/products/product-create-form.ejs')
+    },
+    store: (req, res) => {
+        const nuevoProd = {};
+        nuevoProd.id = products.length + 1;
+        nuevoProd.nombre = req.body.nombre;
+        nuevoProd.resenia = req.body.resenia;
+        nuevoProd.precio = req.body.precio;
+        nuevoProd.clasificacion = req.body.clasificacion;
+        nuevoProd.imagen = req.file.filename;
+        nuevoProd.anioEdicion = req.body.anioEdicion;
+        nuevoProd.fechaPublicacion = req.body.fechaPublicacion;
+        nuevoProd.stock = req.body.stock;
+        nuevoProd.autor = req.body.autor;
+        nuevoProd.editorial = req.body.editorial;
+        nuevoProd.nroPaginas = req.body.nroPaginas;
+        nuevoProd.idioma = req.body.idioma;
+        nuevoProd.isbn = req.body.isbn;
+        products.push(nuevoProd);
+        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '));
+
+        res.redirect('/');
     }
 }
 
--- libreria_G10/src/routes/productRouter.js
+++ libreria_G10/src/routes/productRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router()
 const productsController = require("../controllers/productsController")
+const multer = require("multer");
+const path = require('path');
 
 router.get("/", productsController.detail);
 router.get("/list", productsController.list);
@@ -8,6 +10,24 @@ router.get("/edit", productsController.edit);
 router.get("/delete", productsController.delete);
 router.get("/productDetail", productsController.detail);
 router.get("/all", productsController.verBase);
+
+/*** CREATE ONE PRODUCT ***/
+router.get('/create', productsController.create);
+
+const multerDiskStorage = multer.diskStorage({
+    destination: (req, file, cb) => { // cb= callback
+        let folder = path.join(__dirname, '../../public/images');
+        cb(null, folder);
+    },
+    filename: (req, file, cb) => {
+        let imageName = 'img-' + Date.now() + '-' + (file.originalname);
+        cb(null, imageName);
+    }
+})
+const uploadFile = multer(({ storage: multerDiskStorage })); // multer es un middleware, para implementarlo se almacena en una variable la ejecucín
+
+// queremos procesar este campo= image// single= una única imágen.
+router.post('/', uploadFile.single('imagen'), productsController.store);
 
 
 module.exports = router;
