a9e3f902b0baed130e63a1a1cfb49433dde01593
nachomat85 <ignaciomateu@gmail.com>
2022-07-29 21:47:38 -0300

    subiendo CRUD creación de usuario

--- libreria_G10/src/controllers/usersController.js
+++ libreria_G10/src/controllers/usersController.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const path = require('path');
+const fs = require('fs');
+const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const usersController = {
-    register: (req, res) => {
+    register : (req, res) => {
         res.render('../views/users/register.ejs')
     },
-    login: (req, res) => {
+    login : (req, res) => {
         res.render('../views/users/login.ejs')
     },
+    crearUsuario : (req, res) => {
+        nuevoUsuario = {};
+        nuevoUsuario.id = users.length+1;
+        nuevoUsuario.nombre = req.body.nombre;
+        nuevoUsuario.apellido = req.body.apellido;
+        nuevoUsuario.email = req.body.email;
+        nuevoUsuario.domicilio = req.body.domicilio
+        nuevoUsuario.password = req.body.password
+        nuevoUsuario.tipo = "user"
+
+        users.push(nuevoUsuario)
+        fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
+
+        res.redirect('/')
+    }
 }
 
 module.exports = usersController
--- libreria_G10/src/routes/usersRouter.js
+++ libreria_G10/src/routes/usersRouter.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router()
+var bodyParser = require('body-parser')
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 const usersController = require("../controllers/usersController")
 
 router.get("/register", usersController.register);
 router.get("/login", usersController.login);
+router.post('/', urlencodedParser, usersController.crearUsuario);
 
 module.exports = router;
